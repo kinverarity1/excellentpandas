c8da54b18b3e6c8c866b8b158b202f1e85d81b92
Kent Inverarity <kinverarity@hotmail.com>
2019-08-30 13:03:07 +0930

    Initial commit

---
+++ excellentpandas.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import xlwings as xw
+
+
+def show_in_excel(
+    df, return_df=False, activate_excel=True, print_info=False, max_cols=None
+):
+    """Show DataFrame/Series in Excel."""
+    if print_info:
+        if isinstance(df, pd.DataFrame):
+            print(df.info(max_cols=max_cols))
+        else:
+            print(df.info())
+    if not isinstance(df, pd.DataFrame):
+        tmp_df = df.to_frame()
+    else:
+        tmp_df = df
+    book = xw.Book()
+    book.sheets[0].range("A1").value = tmp_df
+    if activate_excel:
+        book.activate(steal_focus=True)
+    if return_df:
+        return df
+
+
+def via_excel(df):
+    """Show DataFrame/Series in Excel and return the DataFrame/Series.
+
+    This can be added to existing scripts without
+    any change to the script's behaviour:
+
+        >>> from excellentpandas import show_in_excel_pipe
+        >>> result = df.do_something().pipe(show_in_excel_pipe)
+
+    """
+    return show_in_excel(df, return_df=True)
+
+
+def via_info_excel(df, max_cols=None):
+    """Show DataFrame/Series in Excel and return the DataFrame/Series.
+
+    This can be added to existing scripts without
+    any change to the script's behaviour:
+
+        >>> from excellentpandas import show_in_excel_pipe
+        >>> result = df.do_something().pipe(show_in_excel_pipe)
+
+    """
+    return show_in_excel(df, return_df=True, print_info=True, max_cols=max_cols)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="excellentpandas",
+    py_modules=["excellentpandas"],
+    version="0.1.0",
+    description="Load pandas DataFrames in Excel",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kinverarity1/excellentpandas",
+    install_requires=("xlwings", "pandas"),
+)
